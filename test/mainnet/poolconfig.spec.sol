a2dfbe2aeee483257f66c966b5194a821c55a71f
danoctavian <danoctavian91@gmail.com>
2025-11-11 19:50:20 +0200

    curve stableswap test

--- test/mainnet/poolconfig.spec.sol
+++ test/mainnet/poolconfig.spec.sol
@@ -16,6 +16,19 @@ contract VaultBasicFunctionalityTest is BaseIntegrationTest {
     }
 
     function test_create_pool() public {
+        uint256 A = 120;
+        uint256 fee = 3500000;
+        uint256 offpegFeeMultiplier = 120000000000;
+        uint256 ma_exp_time = 1010;
+
+        address poolAddress = create_pool(A, fee, offpegFeeMultiplier, ma_exp_time);
+        console.log("Pool address:", poolAddress);
+    }
+
+    function create_pool(uint256 A, uint256 fee, uint256 offpegFeeMultiplier, uint256 ma_exp_time)
+        public
+        returns (address)
+    {
         address[] memory coins = new address[](2);
         coins[0] = address(0x01Ba69727E2860b37bc1a2bd56999c1aFb4C15D8);
         coins[1] = address(0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f);
@@ -36,15 +49,15 @@ contract VaultBasicFunctionalityTest is BaseIntegrationTest {
             "ynRWAx/GHO-test-pool",
             "TynRWAxGHO",
             coins,
-            120,
-            3500000,
-            120000000000,
-            1010,
+            A,
+            fee,
+            offpegFeeMultiplier,
+            ma_exp_time,
             0,
             assetTypes,
             methodIds,
             oracles
         );
-        console.log("Pool address:", poolAddress);
+        return poolAddress;
     }
 }
